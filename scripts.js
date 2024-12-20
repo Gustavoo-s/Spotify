a2b68a9e400a6c72fb43fd8644b4b2a042302e38
Gustavoo-S <Gullsanttos7890@gmail.com>
2024-12-19 23:05:10 -0300

    Fix: feito o ajuste na barra para poder ficar centralizada na tela

--- scripts.js
+++ scripts.js
@@ -31,9 +31,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         artistCard.innerHTML = `
             <img src="${artist.image}" alt="imagem do ${artist.name}">
-            <h3>${artist.name}</h3>
-            <p>artista</p>
-        `
+            <div>
+                <h3>${artist.name}</h3>
+                <p>artista</p>
+            </div>
+            `
 
         artistGrid.appendChild(artistCard)
     })
@@ -43,10 +45,13 @@ document.addEventListener('DOMContentLoaded', () => {
         albumCard.classList.add('album-card')
 
         albumCard.innerHTML = ` 
+        
         <img src="${album.image}" alt="iamgem do ${album.name}">
-        <p>${album.name}</p>
+        <div>
+            <h3>${album.name}</h3>
+            <p>${album.artist}</p>
+        </div>
         `
-
         albumsGrid.appendChild(albumCard)
     })
 })
